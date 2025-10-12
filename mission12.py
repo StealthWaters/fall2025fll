ec644e7b3b44a33da63def3905673f1151a4a834
StealthWaters <StealthWaters@proton.me>
2025-10-12 12:38:39 -0400

    complete & go 2 home after mission 12
    Current code:
    RUN THIS FIRST, completes mission 12.
    It then goes back to home to re-align.
    
    At the end of the first paragraph of moving, the alignment is very random. To fix this, we made this code to just send it back to home to continue mission 1.

---
+++ mission12.py
@@ -0,0 +1,22 @@
+from hub import port
+import runloop, motor, motor_pair
+motor_pair.pair(motor_pair.PAIR_1, port.E, port.C)
+
+async def main():
+    # The position of the robot should be the right wheel should be just covering the 3rd line from the right of the mission start and the fork all the way back
+    await motor_pair.move_for_degrees(motor_pair.PAIR_1, -380, 0, velocity=500)
+    await motor.run_for_degrees(port.A, 240, 360)
+    await motor_pair.move_for_degrees(motor_pair.PAIR_1, 100, 0, velocity=500)    
+    await motor.run_for_degrees(port.A, -193, 360)
+    # continue
+
+    await motor_pair.move_tank_for_degrees(motor_pair.PAIR_1, 93, -250, 250)
+    await motor_pair.move_for_degrees(motor_pair.PAIR_1, -220, 0, velocity=500)
+    await motor_pair.move_tank_for_degrees(motor_pair.PAIR_1, -95, -250, 250)
+    await motor_pair.move_for_degrees(motor_pair.PAIR_1, -400, 0, velocity=250)
+    await motor_pair.move_tank_for_degrees(motor_pair.PAIR_1, 5, -250, 250)
+    await motor_pair.move_for_degrees(motor_pair.PAIR_1, 300, 0, velocity=500)
+    await motor_pair.move_tank_for_degrees(motor_pair.PAIR_1, 100, -250, 250)
+    await motor_pair.move_for_degrees(motor_pair.PAIR_1, 500, 0, velocity=500)
+
+runloop.run(main())
