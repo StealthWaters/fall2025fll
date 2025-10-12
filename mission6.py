782fdc481bb4d10acf8bb279d422a63f8620d42c
MuffinLord <158377423+PaxtonNhu@users.noreply.github.com>
2025-10-12 13:07:05 -0400

    Update mission6.py

--- mission6.py
+++ mission6.py
@@ -1,4 +1,17 @@
-   await motor.run_for_degrees(port.A, 103, 360) # Raise the fork up to set up for pushing the lever
-    await motor_pair.move_for_degrees(motor_pair.PAIR_1, -100) # Move forward to release the boulders
-    await motor_pair.move_for_degrees(motor_pair.PAIR_1, 100) # Back up
-    await 
+from hub import light_matrix, port
+import runloop, motor, motor_pair
+motor_pair.pair(motor_pair.PAIR_1, port.E, port.C)
+
+async def main():
+    # write your code here
+    # await motor.run_for_degrees(port.A, 190, 360) # Raise the fork up to set up for pushing the lever
+    await motor_pair.move_for_degrees(motor_pair.PAIR_1, -750, 0, velocity=670)
+    await motor_pair.move_tank_for_degrees(motor_pair.PAIR_1, 30, -250, 250) # Turn to sweep to the right
+    await motor.run_for_degrees(port.A, 190, 360) # Raise the fork up to set up for pushing the lever
+    await motor_pair.move_tank_for_degrees(motor_pair.PAIR_1, 30, -250, 250) # Turn to sweep to the right
+#    await motor_pair.move_tank_for_degrees(motor_pair.PAIR_1, 76, -250, 250) # Turn to face the mission after sweeping
+
+    # await motor_pair.move_for_degrees(motor_pair.PAIR_1, 100, 0, velocity=200) # Move forward to release the boulders
+    #await motor_pair.move_for_degrees(motor_pair.PAIR_1, 100, 0, velocity=200) # Back up
+    
+runloop.run(main())
